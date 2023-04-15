8d73e189b1b43d66ccb8a02640b108ea7560fcf8
vivan2011 <81799840+vivan2011@users.noreply.github.com>
2023-04-15 11:46:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function preload(){
+}
+
+function setup() {
+    canvas= createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('Moustache_Filter.jpeg');
+}
